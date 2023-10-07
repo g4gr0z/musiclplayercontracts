36ac1a36dbfbb697bc20ac513fd88d1a7fe82c2d
gagroz <74720366+g4gr0z@users.noreply.github.com>
2023-10-08 03:08:04 +0530

    Create MusicPlayer.sol
    fractionalize

---
+++ MusicPlayer.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./MusicNFT.sol";
+
+contract MusicPlayer is Ownable {
+    ERC20 public fractionalizedToken;  
+    MusicNFT public musicNFT;  
+   
+
+    mapping(uint256 => mapping(address => uint256)) public fractionalOwnership;
+
+    constructor(address _fractionalizedToken, address _musicNFT, address initialOwner) Ownable(initialOwner){
+        fractionalizedToken = ERC20(_fractionalizedToken);
+        musicNFT = MusicNFT(_musicNFT);
+    }
+
+  
+    function purchaseFraction(uint256 tokenId, uint256 amount) external {
+        require(amount > 0, "Amount must be greater than 0");
+        require(musicNFT.ownerOf(tokenId) == address(this), "Music track not held by contract");
+        
+        uint256 totalSupply = fractionalizedToken.totalSupply();
+        uint256 currentOwnership = fractionalOwnership[tokenId][msg.sender];
+        
+        require(totalSupply + amount > totalSupply, "Overflow error");
+        require(totalSupply + amount >= amount, "Underflow error");
+        require(fractionalizedToken.balanceOf(msg.sender) >= amount, "Insufficient balance");
+        
+        fractionalizedToken.transferFrom(msg.sender, address(this), amount);
+        fractionalOwnership[tokenId][msg.sender] += amount;
+
+        emit FractionPurchased(msg.sender, tokenId, amount);
+    }
+
+  
+    function getFractionalOwnership(uint256 tokenId) external view returns (uint256) {
+        return fractionalOwnership[tokenId][msg.sender];
+    }
+
+  
+    function withdraw(uint256 tokenId, uint256 amount) external onlyOwner {
+        require(musicNFT.ownerOf(tokenId) == address(this), "Music track not held by contract");
+        require(fractionalOwnership[tokenId][msg.sender] >= amount, "Insufficient fractional ownership");
+
+        fractionalizedToken.transfer(msg.sender, amount);
+        fractionalOwnership[tokenId][msg.sender] -= amount;
+
+        if (fractionalOwnership[tokenId][msg.sender] == 0) {
+            musicNFT.transferFrom(address(this), msg.sender, tokenId);
+        }
+    }
+
+    event FractionPurchased(address indexed buyer, uint256 tokenId, uint256 amount);
+}
